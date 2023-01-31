82294cc2ce477b208bcb2cf18857637f72f654c9
Jason Lee <jlsync@gmail.com>
2023-01-31 16:27:23 +1100

    full_messages -> to_a

--- lib/action_view/helpers/dynamic_form.rb
+++ lib/action_view/helpers/dynamic_form.rb
@@ -222,7 +222,7 @@ module ActionView
             message = options.include?(:message) ? options[:message] : locale.t(:body)
 
             error_messages = objects.sum do |object|
-              object.errors.full_messages.map do |msg|
+              object.errors.to_a.map do |msg|
                 content_tag(:li, msg)
               end
             end.join.html_safe
